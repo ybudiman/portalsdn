f52a9394701362500eb66e6c3f1f1a06815dcf9b
Yoseph Budiman <yoseph.budiman@sdn.id>
2025-08-14 13:46:36 +0700

    Update release 0.1

--- resources/views/auth/loginuser.blade.php
+++ resources/views/auth/loginuser.blade.php
@@ -44,8 +44,8 @@
                     <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
                         @csrf
                         <div class="logo">
-                            <img src="{{ asset('assets/login/images/logoweb-1.png') }}" alt="easyclass" />
-                            <h4>E-PRESENSI V2</h4>
+                            <img src="{{ asset('assets/logo.png') }}" alt="easyclass" />
+                            <h4>PORTAL</h4>
                         </div>
 
                         <div class="heading">
